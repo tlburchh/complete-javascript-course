7bd3d974dd3609bbdadf60d7748a6f6288c667a2
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-11-30 15:40:59 -0500

    refresher-basics of functions

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -164,6 +164,7 @@ var markJohn
 //  : console.log(firstName + ' drinks juice.');
 
 //  var drink = age >= 18 ? 'beer' : 'juice';
+
 // console.log(drink);
 
 // // if (age >= 18) {
@@ -260,207 +261,26 @@ var markJohn
  * Functions
  */
 
-//  function calculateAge(birthYear) {
-//      return 2018 - birthYear;
-//  }
-
-//  var ageJohn = calculateAge(1990);
-//  var ageMike = calculateAge(1948);
-//  var ageJane = calculateAge(1969);
-//  console.log(ageJohn, ageMike, ageJane);
-
-//  function yearsUntilRetirement(year, firstName) {
-//      var age = calculateAge(year);
-//      var retirement = 65 - age;
-
-//      if (retirement > 0) {
-//      console.log(firstName + ' retires in ' + retirement + ' years ');
-//      } else {
-//          console.log(firstName + ' is already retired')
-//      }
-//     }
-
-//  yearsUntilRetirement(1990, 'John');
-//  yearsUntilRetirement(1948, 'Mike');
-//  yearsUntilRetirement(1969, 'Jane');
-
-/************************
- * Function Statements and Expressions
- */
-
- //function declaration
-//  function whatDoYouDo(job, firstName) {}
-
-
- //function expression
-//  var whatDoYouDo = function(job, firstName) {
-//     switch(job) {
-//         case 'teacher':
-//             return firstName + ' teaches kids how to code';
-//         case 'driver':
-//             return firstName + ' drives a cab';
-//         case 'designer':
-//             return firstName + ' designs websites';
-//         default:
-//             return firstName + ' does something else';
-
-
-//     }
-//  }
-// console.log(whatDoYouDo('teacher', 'John'));
-// console.log(whatDoYouDo('designer', 'Jane'));
-// console.log(whatDoYouDo('retired', 'Mark'));
-
-/********************
- * Arrays
- */
-//Initilialize new array
-//  var names = ['John', 'Mark', 'Jane'];
-//  var years = new Array(1990, 1969, 1948);//possible way but rarely used
-
-//  console.log(names);
-//  console.log(names[0]);
-//  console.log(names.length);
-// // Mutate array data
-//  names[1] = 'Ben';
-// //  names[5] = 'Mary';
-//  names[names.length] = 'Mary'
-//  console.log(names);
-
-//  //Different Data Types
-
-// var john = ['John', 'Smith', 1990, 'teacher', false];
-
-// john.push('blue'); //.push added element to end of array
-// john.unshift('Mr.'); //.unshift added element to beginning of array
-// john.pop(); // removes element from the end
-// john.shift();// removes element from the beginning
-// console.log(john);
-
-// console.log(john.indexOf(1990)); // returns postion of element in array
-//                                   //if not in array returns -1
-
-// var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
-// console.log(isDesigner);
-
-
-/*****************************
- * Coding Challenge 3
- */
-//       MY TRY
-//  function tipCalc(cost){
-//    if (cost < 50){ 
-//      tip = (cost * 0.2);
-//      costAndTip.push('cost'+'tip');
-//    }else if (cost >= 50 && cost <= 200){
-//      tip = (cost * 0.15);
-//      costAndTip.push('cost'+'tip');
-//    }else if(cost > 200) {
-//      tip = (cost * .1);
-//      costAndTip.push('cost'+'tip');
-//    }
-//  }
-//  var cost = [124, 48, 268]
-//  var costAndTip = []
-
-//  console.log(costAndTip)
-///////////////////////////////
-// function tipCalculator(bill) {
-//   var percentage;
-//   if (bill < 50) {
-//       percentage = .2;
-//   } else if (bill >= 50 && bill < 200) {
-//       percentage = .15;
-//   } else {
-//       percentage = .1;
-//   }
-//   return percentage * bill
-// }
-
-// // console.log(tipCalculator(300)); // test
-
-// var bills = [124, 48, 268];
-// var tips = [tipCalculator(bills[0]),
-//             tipCalculator(bills[1]),
-//             tipCalculator(bills[2])];
-// var finalValues = [bills[0] + tips[0],
-//                   bills[1] + tips[1],
-//                   bills[2] + tips[2]];
-// console.log(finalValues)
-
-/*************************
- * Objects and Properties
- */
-
- //Object literal
-// var john = {
-//   firstName: 'John',    // Key: value
-//   lastName: 'Smith',
-//   birthYear: 1990,
-//   family: ['Jane', 'Mark', 'Bob', 'Emily'],
-//   job: 'teacher',
-//   isMarried: false
-// };
-// console.log(john.firstName);
-// console.log(john['lastName']);
-// var x = 'birthYear';
-// console.log(john[x]);
-
-// john.job = 'designer';
-// john['isMarried'] = true;
-// console.log(john);
-
-// // new Object syntax
-// var jane = new Object();
-// jane.name = 'Jane';
-// jane.birthYear = 1969;
-// jane['lastName'] = 'Smith';
-// console.log(jane);
-
-/***********************
- * Objects and Methods
- */
-// var john = {
-//   firstName: 'John',    // Key: value
-//   lastName: 'Smith',
-//   birthYear: 1992,
-//   family: ['Jane', 'Mark', 'Bob', 'Emily'],
-//   job: 'teacher',
-//   isMarried: false,
-//   calcAge: function() {
-//     // return 2018 - this.birthYear;
-//     this.age = 2018 - this.birthYear;
-//   }
-// };
-
-// // console.log(john.calcAge(1990))
-// // john.age = john.calcAge();
-// john.calcAge();
-// console.log(john);
-
-
-/*******************************
- * Coding Challenge 4
- */
-
- var john = {
-name: "John",
-mass: 7,
-height: 7,
-bmi: NaN,
-BMI: function(){
-this.bmi =  this.mass / (this.height*this.height)
+function calculateAge(birthYear) {
+  return 2018 - birthYear;
 }
- };
 
- var mark = {
-   name: "Mark",
-   mass: 6,
-   height: 5,
-   bmi: NaN,
-   BMI: function(){
-     this.bmi = this.mass / (this.height*this.height)
-   }
- }
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJohn, ageMike, ageJane);
 
- 
+function yearsUntilRetirement(year, firstName){
+  var age = calculateAge(year);
+  var retirement = 65 - age;
+
+  if(retirement > 0) {
+  console.log(firstName + ' retires in ' + retirement + 'years.')
+  }else {
+    console.log(firstName + ' is already retired.')
+  }
+}
+
+yearsUntilRetirement(1990, 'John');
+yearsUntilRetirement(1948, 'Mike');
+yearsUntilRetirement(1969, 'Jane');
